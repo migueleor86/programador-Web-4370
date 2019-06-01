7bd5337cfb8104dfef57a2379a8ae50de332aef2
migueleor86 <migueleor86@gmail.com>
2019-06-01 13:54:39 -0300

    Mi primer Commit

---
+++ Clase 01/Alumno/Ejercicio/Js/Index.js
@@ -0,0 +1,22 @@
+// Parte 1
+var nombre = prompt('Ingrese nombre')
+console.log("hola bienvenido: " + nombre)
+
+//Parte 2
+var pi = 3.14
+var numeroASumar = prompt(Insertar Numero a Sumar con PI)
+numeroASumar = parseFloat(numeroASumar)
+console.log(pi + numeroASumar)
+/*
+//Parte 3
+var numeroARestar = prompt("Insertar Numero a Restar con PI")
+numeroARestar = parseFloat(numeroARestar)
+console.log(pi - numeroARestar)
+
+//Parte 4
+
+var firstNumber = prompt('Incenter Primer Numero a sumar')
+firstNumber = parseFloat(firstNumber)
+var secondNumber = prompt('Inserte el segundo numero a sumar')
+secondNumber = parseFloat(secondNumber)
+console.log(firstNumber + secondNumber)*/
