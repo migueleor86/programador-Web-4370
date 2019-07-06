456971e64b7b1b558d915087d01a248f99504d54
migueleor86 <migueleor86@gmail.com>
2019-07-06 14:16:20 -0300

    fin de workshop

--- Clase 10 - 11/Alumno/Workshop/js/index.js
+++ Clase 10 - 11/Alumno/Workshop/js/index.js
@@ -10,30 +10,37 @@ window.onload = function () {
   var mainList = document.getElementById('mainList')
   var deleteDni = document.getElementById('deleteDni')
   var deleteStudentButton = document.getElementById('deleteStudentButton')
+  var searchStudentButton = document.getElementById('searchStudentButton')
+  var searchText = document.getElementById('searchText')
+  var searchList = document.getElementById('searchList')
 
-  // bind events validations
+  // bind events validations, asignamos el evento
   firstName.onblur = validateNotEmpty
   lastName.onblur = validateNotEmpty
   email.onblur = validateEmail
   dni.onblur = validateDni
   addStudentButton.onclick = addStudent
   deleteStudentButton.onclick = deleteStudent
+  searchStudentButton.onclick = searchStudent
 
   // initialize
-  addStudentButton.disabled = true
-  var dataStore = getDataStore(KEY_STORE)
+  addStudentButton.disabled = true // deshabilito el boton , viene habilitado por defecto
+  var dataStore = getDataStore(KEY_STORE) // traer la info de datastore, guardada en forma string
   renderStudentsList(mainList, dataStore)
 
   // render functions
 
-  function renderStudentsList(list, students) {
-    for (var i = 0; i < students.length; i++) {
+  function renderStudentsList(list, students) { // crea una un estudiante en las lista del DOM
+    while (list.hasChildNodes()) { // mientras haya hijos en list
+      list.removeChild(list.firstChild); // remueve el hijo de list
+    }
+    for (var i = 0; i < students.length; i++) { // iterando el data store
       console.log(students[i])
       list.appendChild(createStudentNode(students[i]))
     }
   }
 
-  function createStudentNode(student) {
+  function createStudentNode(student) { // crea un nodos Student y lo devuelve
     var li = document.createElement('li')
     li.id = student.dni
     li.className = 'list-group-item'
@@ -49,7 +56,7 @@ window.onload = function () {
     return li
   }
 
-  function getDataStore(key) {
+  function getDataStore(key) { //
     var store = getLocalList(key)
     if (!store) {
       setLocalList(key, [])
@@ -60,36 +67,59 @@ window.onload = function () {
   }
 
   // data manipulation
-  function addStudent() {
+  function addStudent() { // se ejecuta cuando haga clic en el boton
     var student = getStudentFromForm()
-    dataStore.push(student)
+    dataStore.push(student) // .push mete un elemento nuevo en el array
     // actualiza en el local store la info nueva
-    setLocalList(KEY_STORE, dataStore)
-    clearStudentForm()
-    mainList.appendChild(createStudentNode(student))
-    addStudentButton.disabled = true
+    setLocalList(KEY_STORE, dataStore) // meter el dataStore actulizada en el KEY_STORE
+    clearStudentForm() // limpia la info para no volverla a agregar con el boton
+    mainList.appendChild(createStudentNode(student)) // crear el node para el li creado
+    addStudentButton.disabled = true // deshabilito el boton
   }
 
   function deleteStudent() {
-    // tomar el valor del input dniDelete
+    // tomar el valor del input dniDelete (ya esta declarada)
+
     // buscar ese valor en el dataStore
-    //removeStudentFromDatastore(dataStore, dniDelete.value)
-    // si ese valor existe borrarlo del array
-    // persistir dataStore en localStore 
-    // remover el student del dom 
+    var dniTobedeleted = deleteDni.value // almacena el valor de deleteDni en la var dniTobedeleted
+    if (removeStudentFromDatastore(dataStore, dniTobedeleted)) { // si removeStudentFromDatastore es true
+      // persistir dataStore en localStore 
+      setLocalList(KEY_STORE, dataStore) // persistir, actualiza el localStore reemplanzadola por la info del dara store
+      // remover el student del dom 
+      var liToBeRemoved = document.getElementById(dniTobedeleted)
+      liToBeRemoved.remove()
+    }
   }
 
-  function removeStudentFromDatastore(dataStore, dni) {
+  function searchStudent() {
+    // necesito el texto del student que busco searchText.value
+    var textToBeSearched = searchText.value
+    var studentsWhoMatchSearch = []
+    // buscar en el dataStore los elementos que coincidan con la busqueda
     for (var i = 0; i < dataStore.length; i++) {
       var student = dataStore[i];
-      if (student.dni === dni) {
-        dataStore.splice(i, 1);
-        return
+      var isInFirstName = student.firstName.indexOf(textToBeSearched) > -1
+      var isInLastName = student.lastName.indexOf(textToBeSearched) > -1
+      var isInEmail = student.email.indexOf(textToBeSearched) > -1
+      var isInDni = student.dni.indexOf(textToBeSearched) > -1
+      if (isInFirstName || isInLastName || isInEmail || isInDni) {
+        studentsWhoMatchSearch.push(student)
       }
+      renderStudentsList(searchList, studentsWhoMatchSearch)
     }
   }
 
 
+  function removeStudentFromDatastore(dataStore, dni) {
+    for (var i = 0; i < dataStore.length; i++) {
+      var student = dataStore[i];
+      if (student.dni === dni) { // si el dni almacenado  en el datastor conside con el dni  ingresado
+        dataStore.splice(i, 1); // elimina 1 elemento en la picision i del array, i es la posicion de dni en el array que conincide
+        return true
+      }
+    }
+    return false
+  }
 
   function clearStudentForm() {
     firstName.value = ''
@@ -100,7 +130,7 @@ window.onload = function () {
 
   function getStudentFromForm() {
     return {
-      firstName: firstName.value,
+      firstName: firstName.value, // firstName:  es la key y firstName.value  es valor
       lastName: lastName.value,
       dni: dni.value,
       email: email.value,
@@ -120,16 +150,16 @@ window.onload = function () {
     return true
   }
 
-  function getLocalList(key) {
+  function getLocalList(key) { // verifica la  key
     var nombreKey = typeof key === 'string'
     if (!nombreKey) {
       return false
     }
-    var value = localStorage.getItem(key)
+    var value = localStorage.getItem(key) // interactua con el local store
     if (!value) { // value === null
       return false
     }
-    var parsedArray = JSON.parse(value)
+    var parsedArray = JSON.parse(value) // parsea la info del local store
     Array.isArray(parsedArray)
     if (Array.isArray(parsedArray)) {
       return parsedArray
@@ -146,7 +176,7 @@ window.onload = function () {
     }
   }
 
-  function dniExists(dni) {
+  function dniExists(dni) { // si el dni exite 
     for (var i = 0; i < dataStore.length; i++) {
       if (dataStore[i].dni === dni) {
         return true
